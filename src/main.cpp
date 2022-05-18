71feb44564fa46a201d2b3d68d46a67953075d34
Владислав <perfectionist.one.love@gmail.com>
2022-05-18 22:29:15 +0700

    edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include "simple_vector.hpp"
 
 #include <iostream>
+#include <complex>
 
 int main(int argc, const char *argv[]) {
+
+    using namespace ::std::complex_literals;
+
+    using ::std::complex;
+    using ::std::double_t;
+
+    simple_vector<complex<double_t>> f;
+
+    f.resize(3, std::complex<double_t> { 2i * 1i });
+
+    for (const auto& _elem : f)
+        std::cout
+            << _elem.real() << " "
+            << _elem.imag() << '\n';
 
 
     return 0;
