b6e4750e2d12b238e2f60b4d1ebb7110c2a14da0
Владислав <perfectionist.one.love@gmail.com>
2022-05-19 05:51:20 +0700

    add get_collumn

--- include/simple_matrix.hpp
+++ include/simple_matrix.hpp
@@ -13,6 +13,13 @@ namespace simple {
         simple::vector<T> get_row (std::size_t _row) const {
             return simple::vector<T>(this->at(_row));
         }
+
+        simple::vector<T> get_collumn (std::size_t _col) const {
+            simple::vector<T> _col_v(this->size());
+            for (std::size_t i = 0; i < this->size(); i++)
+                _col_v[i] = (*this)[i][_col];
+            return _col_v;
+        }
     };
 
 }
