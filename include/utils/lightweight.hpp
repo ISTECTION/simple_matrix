56f1559c3fc7d89a23cb6129a951ef9731ff3416
Владислав <perfectionist.one.love@outlook.com>
2022-05-29 21:53:36 +0700

    edit lightweight.hpp

--- include/utils/lightweight.hpp
+++ include/utils/lightweight.hpp
@@ -3,15 +3,13 @@
 #include <cmath>            /// sqrt
 
 namespace utils {
-
     #if defined(__cpp_generic_lambdas) && __cpp_generic_lambdas >= 201707L
-    constexpr auto EQUAL = [] <typename T> (T a, T b) { return (abs(a - b) < 1E-8); };
+    constexpr auto EQUAL = []<typename T>(T a, T b) { return (abs(a - b) < 1E-8); };
     #elif defined(__cpp_constexpr) && __cpp_constexpr >= 201603L
     constexpr auto EQUAL = [](auto a, auto b) { return (abs(a - b) < 1E-8); };
     #else
     auto EQUAL = [](auto a, auto b) { return (abs(a - b) < 1E-8); };
     #endif
-
 }
 
 #endif /// _LIGHTWEIGHT_HPP
