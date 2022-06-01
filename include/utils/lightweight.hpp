4fd1d5563a24d6bff54bf7e79a9ebf92e199e509
Владислав <perfectionist.one.love@outlook.com>
2022-06-02 00:00:07 +0700

    edit lightweight.hpp

--- include/utils/lightweight.hpp
+++ include/utils/lightweight.hpp
@@ -1,15 +1,12 @@
 #ifndef _LIGHTWEIGHT_HPP
 #define _LIGHTWEIGHT_HPP
-#include <cmath>            /// sqrt
+#include <cmath>            /// abs
 
 namespace utils {
-    #if defined(__cpp_generic_lambdas) && __cpp_generic_lambdas >= 201707L
     constexpr auto EQUAL = []<typename T>(T a, T b) -> bool { return (abs(a - b) < 1E-8); };
-    #elif defined(__cpp_constexpr) && __cpp_constexpr >= 201603L
-    constexpr auto EQUAL = [](auto a, auto b) -> bool { return (abs(a - b) < 1E-8); };
-    #else
-    auto EQUAL = [](auto a, auto b) -> bool { return (abs(a - b) < 1E-8); };
-    #endif
+
+    // #if   defined(__cpp_generic_lambdas) && __cpp_generic_lambdas >= 201707L     # typename
+    // #elif defined(__cpp_constexpr)       && __cpp_constexpr       >= 201603L     # constexpr
 }
 
 #endif /// _LIGHTWEIGHT_HPP
